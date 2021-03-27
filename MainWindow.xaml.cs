434d9f56441ba21c8bdea07cb174dcd7c6c890cb
Lolcoman <michaelkozak.cz@gmail.com>
2021-03-27 23:25:15 +0100

    prvni vytvoreni

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dama_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Ukončení aplikace
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ExitGameMenu_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Opravdu chcete ukončit hru?", "Ukončit hru", MessageBoxButton.YesNo);
+            if (result == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+        private void LoadGameMenu_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Save game file|*.txt";
+            if (dialog.ShowDialog() == true)
+            {
+                //zde se bude načítat hra z .txt
+                //game.Load(dialog.FileName)
+                MessageBox.Show(dialog.FileName);
+            }
+        }
+
+        private void NewGameMenu_Click(object sender, RoutedEventArgs e)
+        {
+            NewGame newGame = new NewGame();
+            newGame.Owner = this; //hlavní okno je vlastníkem tohoto okna
+            newGame.ShowDialog();
+        }
+    }
+}
